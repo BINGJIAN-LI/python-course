8d7c2b1a444de5e7ce9d14a5705ab68881b5c72b
bingjian li <lbj981104@gmail.com>
2019-10-18 19:18:46 +0100

    home work 1

---
+++ assignment 1.py
@@ -0,0 +1,26 @@
+
+# Q 2
+print(25)
+
+# Q 3
+print(0b10)
+# Q 4
+print(0o10)
+# Q 5
+print(0x10)
+# Q 6
+print(type(25.67))
+# Q 7
+print(4e5)
+# Q 8
+print("This is a string with double quotes")
+# Q 9
+print('This is a string with single quotes')
+# Q 10
+print("This is a string that contains a single quote")
+# Q 11
+print('This is a string that contains a double quote')
+# Q 12
+print("This is a string with double quotes that contains a double quote")
+# Q13
+print("This a multiline output\nThat contains two lines")
